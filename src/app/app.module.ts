093d51f5bb8d010d34f581b9487b4e123f3dcd46
sfujimoto <unicoshun@gmail.com>
2016-10-14 20:50:26 +0900

    ~#204 add ngmodule child

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,44 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header.component';
-import { RecipesComponent } from './recipes/recipes.component';
-import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
-import { RecipeItemComponent } from './recipes/recipe-list/recipe-item.component';
-import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
-import { ShoppingListComponent } from './shopping-list/shopping-list.component';
-import { ShoppingListAddComponent } from './shopping-list/shopping-list-add.component';
-import { DropdownDirective } from './dropdown.directive';
+
 
 import { RecipeService } from './recipes/recipe.service';
 import { ShoppingListService } from './shopping-list/shopping-list.service';
-import {routing} from "./app.routing";
-import {RecipeStartComponent} from "./recipes/recipe-start.component";
-import {RecipeEditComponent} from "./recipes/recipe-edit/recipe-edit.component";
+import { routing } from "./app.routing";
+import {CoreModule} from "./core.module";
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
-    RecipesComponent,
-    RecipeListComponent,
-    RecipeItemComponent,
-    RecipeDetailComponent,
-    RecipeStartComponent,
-    RecipeEditComponent,
-    ShoppingListComponent,
-    ShoppingListAddComponent,
-    DropdownDirective
+
+    HeaderComponent
+
   ],
   imports: [
     BrowserModule,
-    FormsModule,
-    ReactiveFormsModule,
     HttpModule,
-    routing
+    routing,
+    CoreModule
   ],
   providers: [RecipeService, ShoppingListService],
   bootstrap: [AppComponent]
