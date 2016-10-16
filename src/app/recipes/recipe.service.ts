caf17685b3369aa2b0d26f8f99cc36423b6572b9
sfujimoto <unicoshun@gmail.com>
2016-10-16 10:39:11 +0900

    add assets

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -9,19 +9,19 @@ import 'rxjs/Rx';
 export class RecipeService {
   recipesChanged = new EventEmitter<Recipe[]>();
   private recipes: Recipe[] = [
-    new Recipe('Curry', 'Very tasty', 'http://bigoven-res.cloudinary.com/image/upload/t_recipe-256/thai-shrimp-curry-17.jpg', [
+    new Recipe('Curry', 'Very tasty', '../../assets/curry.jpg', [
       new Ingredient('French Fries', 2),
       new Ingredient('Pork Fries', 1)
     ]),
-    new Recipe('Sushi', 'Good!', 'http://english-master.info/img/sushi-1-icon.png', [
+    new Recipe('Sushi', 'Good!', '../../assets/sushi.jpg', [
       new Ingredient('Squid', 4),
       new Ingredient('Rice', 5)
     ]),
-    new Recipe('Takoyaki', 'The sauce!', 'http://jpninfo.com/wp-content/uploads/2015/09/takoyaki1.jpg', [
+    new Recipe('Takoyaki', 'The sauce!', '../../assets/takoyaki.jpg', [
       new Ingredient('Octopus', 2),
       new Ingredient('Flour', 3)
     ]),
-    new Recipe('Sukiyaki', 'Very good!', 'https://media-cdn.tripadvisor.com/media/photo-s/07/d4/75/61/tokyo-sukiyaki-tei.jpg', [
+    new Recipe('Sukiyaki', 'Very good!', '../../assets/sukiyaki.jpg', [
       new Ingredient('Beef', 2),
       new Ingredient('Sauce', 3)
     ])
