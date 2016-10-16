7f73415d5ae15226fd18eb6c1b94f3716a3476bf
sfujimoto <unicoshun@gmail.com>
2016-10-16 12:15:09 +0900

    add images in assets

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -9,19 +9,19 @@ import 'rxjs/Rx';
 export class RecipeService {
   recipesChanged = new EventEmitter<Recipe[]>();
   private recipes: Recipe[] = [
-    new Recipe('Curry', 'Very tasty', '../../assets/curry.jpg', [
+    new Recipe('Curry', 'Very tasty', '../../assets/images/curry.jpg', [
       new Ingredient('French Fries', 2),
       new Ingredient('Pork Fries', 1)
     ]),
-    new Recipe('Sushi', 'Good!', '../../assets/sushi.jpg', [
+    new Recipe('Sushi', 'Good!', '../../assets/images/sushi.jpg', [
       new Ingredient('Squid', 4),
       new Ingredient('Rice', 5)
     ]),
-    new Recipe('Takoyaki', 'The sauce!', '../../assets/takoyaki.jpg', [
+    new Recipe('Takoyaki', 'The sauce!', '../../assets/images/takoyaki.jpg', [
       new Ingredient('Octopus', 2),
       new Ingredient('Flour', 3)
     ]),
-    new Recipe('Sukiyaki', 'Very good!', '../../assets/sukiyaki.jpg', [
+    new Recipe('Sukiyaki', 'Very good!', '../../assets/images/sukiyaki.jpg', [
       new Ingredient('Beef', 2),
       new Ingredient('Sauce', 3)
     ])
